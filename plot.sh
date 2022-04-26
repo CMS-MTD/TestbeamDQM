015fb8abf7a166a4168e2ead0813c9b7878bde8a
Ohannes Kamer Koseyan <kaderci.johannes@gmail.com>
2022-04-26 14:20:24 -0500

    Input file path moved to a single variable.

--- plot.sh
+++ plot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+P="/eos/uscms/store/group/cmstestbeam/SensorBeam2022/LecroyScope/RecoData/TimingDAQRECO/RecoWithTracks/v10"
 if [ "$1" == "" ] || [ "$2" == "" ]; then
 echo "## PLOTTER.SH - WATCH SCRIPT FOR DQM PLOTS ##"
 echo "Usage"
@@ -12,9 +13,9 @@ while true
 do
 if [ $2 -lt 3 ];
 then
-n=$(ls /eos/uscms/store/group/cmstestbeam/SensorBeam2022/LecroyScope/RecoData/TimingDAQRECO/RecoWithTracks/v3 | tail -n $2 | sed 's/[^0-9]//g' | sed ':a;N;$!ba;s/\n/ /g')
+n=$(ls $P | tail -n $2 | sed 's/[^0-9]//g' | sed ':a;N;$!ba;s/\n/ /g')
 else
-n=$(ls /eos/uscms/store/group/cmstestbeam/SensorBeam2022/LecroyScope/RecoData/TimingDAQRECO/RecoWithTracks/v3 | tail -n $2 | sed 2,$(($2-1))d | sed 's/[^0-9]//g' | sed ':a;N;$!ba;s/\n/ /g')
+n=$(ls $P | tail -n $2 | sed 2,$(($2-1))d | sed 's/[^0-9]//g' | sed ':a;N;$!ba;s/\n/ /g'3)
 fi
 if [ $2 -eq 1 ];
 then
